afb81ae9ff463e7c627b0fb23635a05287f06d26
AreejFA <44033296+AreejFA@users.noreply.github.com>
2020-02-17 10:17:01 +0300

    Initial Commit

---
+++ I am rich ios13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich ios13
+//
+//  Created by Areej on 2/17/20.
+//  Copyright © 2020 Areej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
